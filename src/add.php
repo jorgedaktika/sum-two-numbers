109cb3e3ef47dfcbcd1c88e9c5a2a06ca474bea1
jorgedaktika <jorge@daktika.com>
2023-01-03 23:14:02 +0100

    first commit

---
+++ src/add.php
@@ -0,0 +1,13 @@
+<?php
+//namespace App;
+
+// class MyClass2 {
+//     public function add($a, $b){
+//         return $a + $b;
+//     }
+// }
+
+// public function multi($a, $b){
+//     return $a * $b;
+// }
+?>
---
+++ test/addTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Test;
+
+//use App\MyClass;
+
+class MyClass {
+    public function add($a, $b){
+        return $a / $b;
+    }
+}
+
+class Test1Test extends \PHPUnit\Framework\TestCase {
+
+	public function testAdd(): void  {
+		$myObj = new MyClass();
+		$this->assertEquals($myObj->add(1,2), 3);
+		$this->assertEquals($myObj->add(-1,4), 3);
+		$this->assertEquals($myObj->add(0,0), 0);
+	}
+
+}
+?>
